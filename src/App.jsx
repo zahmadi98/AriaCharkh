f768a5861a3c97217cbf61356528d26d599af8e2
zeinab <ms.zeinab.ahmadi@gmail.com>
2025-09-27 11:58:06 +0330

    add navbar with SearchBox and Logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import StatusBar from "./components/StatusBar";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100">
-    
-    <div className="w-full fixed px-20 py-2 bg-[#FCDEE0] h-[3.688rem]"></div>
+      <div className="w-full px-20 py-2 bg-[#FCDEE0] h-[3.688rem]"></div>
       <StatusBar />
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import logo from '../assets/logo.svg'
+import { User, ShoppingCart, SearchNormal1, ArrowDown3 } from "iconsax-react";
+
+const Navbar = () => {
+  return (
+    <div className="w-full h-[4.125rem] px-20 py-2 flex items-center justify-between bg-white shadow">
+        <div className="flex items-center gap-4">
+            <ShoppingCart size={20} color="#000" className="cursor-pointer" />
+            <span className="text-gray-400">|</span>
+            <div className="flex items-center gap-1">
+                <ArrowDown3 size="17" color="#000" className="cursor-pointer"/>
+                <User size={20} color="#000" className="cursor-pointer" />
+            </div>
+        </div>
+
+        <div className="flex items-center gap-4">
+
+            <div className="w-[31.688rem] relative">
+                <input
+                 type="text"
+                 placeholder="دنبال چه محصولی هستی؟"
+                 className="w-full text-xs text-right pl-10 py-2 px-3 rounded-lg border bg-[#EDEDED] focus:outline-none focus:ring-2"
+                />
+                <div className="absolute left-3 top-1/2 -translate-y-1/2 pointer-events-none">
+                    <SearchNormal1 size="18" color="#6B7280" />
+                </div>
+            </div>
+
+            <img src={logo} alt="Logo" className="h-12 w-auto" />
+        </div>
+
+    </div>
+  );
+};
+
+export default Navbar;
